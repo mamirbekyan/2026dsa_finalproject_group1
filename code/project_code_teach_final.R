5275975e620e3bf58a38af8a77d67237dd8d318a
Mari Amirbekyan <amirbekyan.mari@gmail.com>
2026-05-01 12:55:05 -0400

    final again i guess

--- code/project_code_teach_final.R
+++ code/project_code_teach_final.R
@@ -1,11 +1,14 @@
-# shared_lib_path <- "/work/crss8030/instructor_data/shared_R_libs"
+#shared_lib_path <- "/work/crss8030/instructor_data/shared_R_libs"
 
 #class_packages <- c("tidymodels", "tidyverse", "vip", "ranger", "finetune", "parsnip", "reticulate", "xgboost", "doParallel", "lme4", "here")
 
 #install.packages(class_packages, lib = shared_lib_path)
-
+#install.packages("caret")
 #install.packages("lightgbm")
-# .libPaths(c("/work/crss8030/instructor_data/shared_R_libs", .libPaths()))
+#install.packages("janitor")
+#install.packages("USAboundaries")
+#install.packages("sf")
+#.libPaths(c("/work/crss8030/instructor_data/shared_R_libs", .libPaths()))
 
 library(tidyverse)
 library(lightgbm)
@@ -54,13 +57,13 @@ fieldweather_submission <- read_csv(here("data", "fieldweatherdata_testing.csv")
 training_soil1 <- training_soil %>%
   select(!(year)) %>%
   separate(site,
-                   into = c("site", "year")) %>%
+           into = c("site", "year")) %>%
   mutate(year = as.numeric(year)) %>%
   filter(nchar(as.character(year)) == 4) 
 
 training_metadata1<- training_metadata %>%
   separate(site, 
-                  into = c("site", "x")) %>%
+           into = c("site", "x")) %>%
   select(!(x)) %>% #remove x column
   unique() %>%
   filter(
@@ -73,7 +76,7 @@ write_csv(training_metadata1,
 
 training_trait1 <- training_trait %>%
   separate(site, 
-                  into = c("site", "x")) %>%
+           into = c("site", "x")) %>%
   select(!(x)) %>% #remove x column with the -Dry, -Early, -Late
   mutate(
     across(c(date_planted, date_harvested),
@@ -97,13 +100,13 @@ library(ggplot2)
 library(dplyr)
 library(sf)
 site_locations <- us_states() %>%
-filter(!(state_abbr %in% c("PR", "AK", "HI")))
+  filter(!(state_abbr %in% c("PR", "AK", "HI")))
 distributions_usa <- ggplot() +
   geom_sf(data = site_locations) +
   geom_point(data = training_merged, 
              aes(x = longitude,
                  y = latitude,
-                color = yield_mg_ha)
+                 color = yield_mg_ha)
   )
 
 ggsave(
@@ -132,7 +135,7 @@ write_csv(training_merged1,
 
 testing_submission1 <- testing_submission %>%
   filter(!(site %in% c("ONH3")))
-  
+
 write_csv(training_merged1,
           here::here("data", "testing", "testing_submission1.csv"))
 #confirm: 
@@ -163,13 +166,10 @@ fieldweatherdata %>%
 yield_density_all_years <- training_merged1 %>%
   ggplot(aes(x = yield_mg_ha)) +
   geom_density() +
-  labs(
-    title = "Density of Yield Across All Years",
-    x = "Yield (mg/ha)",
-    y = "Density")
- 
+  labs(title = "Density of Yield Across All Years", x = "Yield (mg/ha)", y = "Density")
+
 ggsave(
-  plot = yeild_density_all_years,
+  plot = yield_density_all_years,
   filename = here ("teach_output", "png", "yield_density_all_years.png"),
   height = 6,
   width = 9,
@@ -179,7 +179,7 @@ ggsave(
 fieldweather1 <- fieldweatherdata %>%
   # Selecting needed variables
   dplyr::select(-tile, -altitude) %>%
-# Creating a date class variable  
+  # Creating a date class variable
   mutate(date_chr = paste0(year, "/", yday)) %>% #paste0 helps to merge columns together
   mutate(date = as.Date(date_chr, "%Y/%j"))
 
@@ -188,15 +188,15 @@ weather_features <- training_merged1 %>%
   select(site, year, date_planted, date_harvested) %>%
   distinct() %>%
   left_join(fieldweather1, by = c("site", "year")) %>%
-  filter(date >= date_planted,
-         date <= date_harvested) %>%
+  filter(date >= date_planted, date <= date_harvested) %>%
   group_by(site, year, date_planted, date_harvested) %>%
   summarise(
     n_days = n(),
     sum_gdd = sum(gdd_rounded, na.rm = TRUE),
     across(
       c(dayl_s, srad_w_m_2, tmax_deg_c, tmin_deg_c, vp_pa),
-      mean, na.rm = TRUE,
+      mean,
+      na.rm = TRUE,
       .names = "mean_{.col}"
     ),
     sum_precip = sum(prcp_mm_day, na.rm = TRUE),
@@ -207,11 +207,10 @@ weather_merged <- training_merged1 %>%
   filter(year != 2014) %>%
   left_join(weather_features,
             by = c("site", "year", "date_planted", "date_harvested")) %>%
-  select(-id, replicate, block)
+  select(-id)
 
 
-write_csv(weather_merged,
-          here::here("data", "weather_merged.csv"))
+write_csv(weather_merged, here::here("data", "weather_merged.csv"))
 
 #Plotting all variables by site
 
@@ -219,28 +218,31 @@ allplots <- weather_merged %>%
   pivot_longer(sum_gdd:sum_precip) %>%
   group_by(name) %>%
   nest() %>%
-  mutate(plot = map2(data, name,
-                     ~ggplot(data = .x,
-       aes(x = value,
-           y = site,
-           fill = stat(x))
-       ) +
-  geom_density_ridges_gradient(scale = 3,
-                               rel_min_height = 0.01) +
-  scale_fill_viridis_c(option = "C", alpha = .8) +
-  theme(legend.position = "none") +
-  labs(x= .y,
-       y = "site")))
-  
-walk2(allplots$plot, 
-      allplots$name, 
-      ~ ggsave( 
-        filename = here("teach_output", "png", 
-                        paste0(.y, ".png")), 
-        plot = .x, 
-        height = 6, 
-        width = 9, 
-        dpi = 150 ) )
+  mutate(
+    plot = map2(
+      data,
+      name,
+      ~ ggplot(data = .x, aes(
+        x = value, y = site, fill = stat(x)
+      )) +
+        geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01) +
+        scale_fill_viridis_c(option = "C", alpha = .8) +
+        theme(legend.position = "none") +
+        labs(x = .y, y = "site")
+    )
+  )
+
+walk2(
+  allplots$plot,
+  allplots$name,
+  ~ ggsave(
+    filename = here("teach_output", "png", paste0(.y, ".png")),
+    plot = .x,
+    height = 6,
+    width = 9,
+    dpi = 150
+  )
+)
 
 set.seed(76332)
 
@@ -256,12 +258,8 @@ merged_test <- testing(merged_split)
 merged_test
 
 density_plot_train_to_test_set <- ggplot() +
-  geom_density(data = merged_train, 
-               aes(x = yield_mg_ha),
-               color = "red") +
-  geom_density(data = merged_test, 
-               aes(x = yield_mg_ha),
-               color = "blue") 
+  geom_density(data = merged_train, aes(x = yield_mg_ha), color = "red") +
+  geom_density(data = merged_test, aes(x = yield_mg_ha), color = "blue")
 
 
 ggsave(
@@ -293,13 +291,13 @@ get_from_env("models")
 
 ###setting up xgboost
 
-xgb_spec <- 
+xgb_spec <-
   boost_tree(
     trees = tune(),
     tree_depth = tune(),
     min_n = tune(),
     learn_rate = tune()
-    ) %>%
+  ) %>%
   set_engine ("xgboost") %>%
   set_mode("regression")
 
@@ -309,33 +307,24 @@ xgb_spec
 set.seed(235)
 resampling_foldcv <- vfold_cv(merged_train, v = 5)
 
-### These are removed below because the computational speed is heavily compromised. However, it would improve accuracy if left in. 
+### These are removed below because the computational speed is heavily compromised. However, it would improve accuracy if left in.
 
 # Create leave one year out cv object from the sampling data
 #resampling_fold_loyo <- group_vfold_cv(merged_train,
-                                    #   group = year)
+#   group = year)
 
 # Create leave one location out cv object from the sampling data
 #resampling_fold_loso <- group_vfold_cv(merged_train,
-                                      # group = site)
+# group = site)
 
 
 ###Sampling using Latin-Hypercube
 set.seed(12345)
-xgb_grid <- grid_latin_hypercube(
-  tree_depth(),
-  min_n(),
-  learn_rate(),
-  trees(),
-  size = 5
-)
+xgb_grid <- grid_latin_hypercube(tree_depth(), min_n(), learn_rate(), trees(), size = 5)
 xgb_grid
 
-xgb_latin_hyper_plot <- ggplot(data = xgb_grid,
-       aes(x = tree_depth, 
-           y = min_n)) +
-  geom_point(aes(color = factor(learn_rate),
-                 size = trees),
+xgb_latin_hyper_plot <- ggplot(data = xgb_grid, aes(x = tree_depth, y = min_n)) +
+  geom_point(aes(color = factor(learn_rate), size = trees),
              alpha = .5,
              show.legend = FALSE)
 
@@ -350,7 +339,8 @@ ggsave(
 ### Adaptive Grid Search to Identify best model later
 n_cores <- as.numeric(Sys.getenv("SLURM_CPUS_ON_NODE"))
 
-if (is.na(n_cores)) n_cores <- parallel::detectCores() - 1
+if (is.na(n_cores))
+  n_cores <- parallel::detectCores() - 1
 
 # Start the Cluster
 cl <- makePSOCKcluster(n_cores)
@@ -367,8 +357,8 @@ results_xgb <- tune_race_anova(
   preprocessor = merged_recipe,
   resamples = resampling_foldcv,
   grid = xgb_grid,
-  control = control_race(save_pred = TRUE,
-    parallel_over = "everything"))
+  control = control_race(save_pred = TRUE, parallel_over = "everything")
+)
 
 stopCluster(cl)
 
@@ -394,9 +384,7 @@ final_spec_xgb
 
 ###Validation
 set.seed(10)
-final_fit_xgb <- last_fit(final_spec_xgb,
-                merged_recipe,
-                split = merged_split)
+final_fit_xgb <- last_fit(final_spec_xgb, merged_recipe, split = merged_split)
 final_fit_xgb %>%
   collect_predictions() #provides predictions on the test set
 final_fit_xgb %>%
@@ -405,13 +393,13 @@ final_fit_xgb %>%
 
 ###setting up LightGBM
 
-lgbm_spec <- 
+lgbm_spec <-
   boost_tree(
     trees = tune(),
     tree_depth = tune(),
     min_n = tune(),
     learn_rate = tune()
-    ) %>%
+  ) %>%
   set_engine ("lightgbm") %>%
   set_mode("regression")
 
@@ -423,20 +411,11 @@ resampling_foldcv_lgbm <- vfold_cv(merged_train, v = 5)
 
 ###Sampling using Latin-Hypercube
 set.seed(123456)
-lgbm_latin_hyper_plot <- grid_latin_hypercube(
-  tree_depth(),
-  min_n(),
-  learn_rate(),
-  trees(),
-  size = 5
-)
+lgbm_latin_hyper_plot <- grid_latin_hypercube(tree_depth(), min_n(), learn_rate(), trees(), size = 5)
 
 
-ggplot(data = lgbm_latin_hyper_plot,
-       aes(x = tree_depth, 
-           y = min_n)) +
-  geom_point(aes(color = factor(learn_rate),
-                 size = trees),
+ggplot(data = lgbm_latin_hyper_plot, aes(x = tree_depth, y = min_n)) +
+  geom_point(aes(color = factor(learn_rate), size = trees),
              alpha = .5,
              show.legend = FALSE)
 ggsave(
@@ -449,7 +428,8 @@ ggsave(
 ### Adaptive Grid Search to Identify best model later
 n_cores <- as.numeric(Sys.getenv("SLURM_CPUS_ON_NODE"))
 
-if (is.na(n_cores)) n_cores <- parallel::detectCores() - 1
+if (is.na(n_cores))
+  n_cores <- parallel::detectCores() - 1
 
 # Start the Cluster
 cl <- makePSOCKcluster(n_cores)
@@ -466,8 +446,8 @@ results_lgbm <- tune_race_anova(
   preprocessor = merged_recipe,
   resamples = resampling_foldcv_lgbm,
   grid = lgbm_grid,
-  control = control_race(save_pred = TRUE,
-    parallel_over = "everything"))
+  control = control_race(save_pred = TRUE, parallel_over = "everything")
+)
 
 stopCluster(cl)
 
@@ -493,9 +473,7 @@ final_spec_lgbm
 
 ###Validation
 set.seed(10)
-final_fit_lgbm <- last_fit(final_spec_lgbm,
-                merged_recipe,
-                split = merged_split)
+final_fit_lgbm <- last_fit(final_spec_lgbm, merged_recipe, split = merged_split)
 final_fit_lgbm %>%
   collect_predictions() #provides predictions on the test set
 final_fit_lgbm %>%
@@ -503,9 +481,12 @@ final_fit_lgbm %>%
   mutate(estimate = round(.estimate, 3))
 
 final_spec_lgbm <- boost_tree(
-  trees = best_r2_lgbm$trees,           # Number of boosting rounds (trees)
-  tree_depth = best_r2_lgbm$tree_depth, # Maximum depth of each tree
-  min_n = best_r2_lgbm$min_n,           # Minimum number of samples to split a node
+  trees = best_r2_lgbm$trees,
+  # Number of boosting rounds (trees)
+  tree_depth = best_r2_lgbm$tree_depth,
+  # Maximum depth of each tree
+  min_n = best_r2_lgbm$min_n,
+  # Minimum number of samples to split a node
   learn_rate = best_r2_lgbm$learn_rate  # Learning rate (step size shrinkage)
 ) %>%
   set_engine("lightgbm") %>%
@@ -514,9 +495,7 @@ final_spec_lgbm <- boost_tree(
 final_spec_lgbm
 
 set.seed(10)
-final_fit_lgbm <- last_fit(final_spec_lgbm,
-                merged_recipe,
-                split = merged_split)
+final_fit_lgbm <- last_fit(final_spec_lgbm, merged_recipe, split = merged_split)
 
 final_fit_lgbm %>%
   collect_predictions()
@@ -526,13 +505,12 @@ final_fit_lgbm %>%
 
 Publication_ready_xgb <- final_fit_xgb %>%
   collect_predictions() %>%
-  ggplot(aes(x = yield_mg_ha,
-             y = .pred)) +
+  ggplot(aes(x = yield_mg_ha, y = .pred)) +
   geom_point() +
   geom_abline() +
   geom_smooth(method = "lm") +
   scale_x_continuous(limits = c(20, 40)) +
-  scale_y_continuous(limits = c(20, 40)) 
+  scale_y_continuous(limits = c(20, 40))
 
 ggsave(
   plot = Publication_ready_xgb,
@@ -544,8 +522,7 @@ ggsave(
 
 Publication_ready_lgbm <- final_fit_lgbm %>%
   collect_predictions() %>%
-  ggplot(aes(x = yield_mg_ha,
-             y = .pred)) +
+  ggplot(aes(x = yield_mg_ha, y = .pred)) +
   geom_point() +
   geom_abline() +
   geom_smooth(method = "lm") +
@@ -562,22 +539,19 @@ ggsave(
 
 #XGBoost
 vip_xgb <- final_spec_xgb %>%
-  fit(yield_mg_ha ~ .,
-         data = bake(merged_prep, merged_train)) %>% 
-    vi() %>%
-  mutate(
-    Variable = fct_reorder(Variable, 
-                           Importance)
-  ) %>%
-  ggplot(aes(x = Importance, 
-             y = Variable)) +
+  fit(yield_mg_ha ~ ., data = bake(merged_prep, merged_train)) %>%
+  vi() %>%
+  mutate(Variable = fct_reorder(Variable, Importance)) %>%
+  ggplot(aes(x = Importance, y = Variable)) +
   geom_col() +
   scale_x_continuous(expand = c(0, 0)) +
   labs(y = NULL) +
   theme(panel.background  = element_rect(fill = "gray82"),
         panel.grid = element_blank())
 
-cat(paste0("\nSaving...... publication ready plot for variable importance\n"))
+cat(paste0(
+  "\nSaving...... publication ready plot for variable importance\n"
+))
 
 ggsave(
   plot = vip_xgb,
@@ -589,22 +563,19 @@ ggsave(
 
 #LightGBM
 vip_lgbm <- final_spec_lgbm %>%
-  fit(yield_mg_ha ~ .,
-         data = bake(merged_prep, merged_train)) %>% 
-    vi() %>%
-  mutate(
-    Variable = fct_reorder(Variable, 
-                           Importance)
-  ) %>%
-  ggplot(aes(x = Importance, 
-             y = Variable)) +
+  fit(yield_mg_ha ~ ., data = bake(merged_prep, merged_train)) %>%
+  vi() %>%
+  mutate(Variable = fct_reorder(Variable, Importance)) %>%
+  ggplot(aes(x = Importance, y = Variable)) +
   geom_col() +
   scale_x_continuous(expand = c(0, 0)) +
   labs(y = NULL) +
   theme(panel.background  = element_rect(fill = "gray82"),
         panel.grid = element_blank())
 
-cat(paste0("\nSaving...... publication ready plot for variable importance\n"))
+cat(paste0(
+  "\nSaving...... publication ready plot for variable importance\n"
+))
 
 ggsave(
   plot = vip_lgbm,
@@ -647,15 +618,20 @@ comparison_table <- all_metrics %>%
 
 comparison_table
 
-write_csv(comparison_table,
-          here::here("teach_output", "csv", "model_comparison_metrics.csv"))
+write_csv(
+  comparison_table,
+  here::here("teach_output", "csv", "model_comparison_metrics.csv")
+)
 
 
 #creating an average for the growing window so that we can clculate GDD based on this range later. Created then renamed to the same column name as training_merged1 for consistency
 average_growing_window_testing_submission <- training_merged1 %>%
   group_by(year, site, hybrid) %>%
   summarise(
-    day_of_year_planted = round(mean(day_of_year_planted,na.rm = TRUE)),                          day_of_year_harvested = round(mean(day_of_year_harvested,na.rm = TRUE)), .groups = "drop")%>%
+    day_of_year_planted = round(mean(day_of_year_planted, na.rm = TRUE)),
+    day_of_year_harvested = round(mean(day_of_year_harvested, na.rm = TRUE)),
+    .groups = "drop"
+  ) %>%
   mutate(
     date_planted   = ymd(paste0(year, "-01-01")) + days(day_of_year_planted - 1),
     date_harvested = ymd(paste0(year, "-01-01")) + days(day_of_year_harvested - 1)
@@ -666,8 +642,7 @@ average_growing_window_testing_submission
 
 #Wrangling testing soil to separate year from site in the site column before merging for prediction
 testing_soil1 <- testing_soil %>%
-  separate(site, 
-                  into = c("site", "x")) %>%
+  separate(site, into = c("site", "x")) %>%
   select(!(x)) %>%
   unique()
 
@@ -678,8 +653,7 @@ merged_submission <- testing_submission %>%
   select(-previous_crop)
 
 merged_submission_final <- merged_submission %>%
-  left_join(
-    average_growing_window_testing_submission) %>%
+  left_join(average_growing_window_testing_submission) %>%
   mutate(
     date_planted   = as.Date(date_planted),
     date_harvested = as.Date(date_harvested)
@@ -688,7 +662,7 @@ merged_submission_final <- merged_submission %>%
 fieldweather_submission1 <- fieldweather_submission %>%
   # Selecting needed variables
   dplyr::select(-tile, -altitude) %>%
-# Creating a date class variable  
+  # Creating a date class variable
   mutate(date_chr = paste0(year, "/", yday)) %>%
   mutate(date = as.Date(date_chr, "%Y/%j"))
 
@@ -702,15 +676,15 @@ weather_features_submission1 <- merged_submission_final1 %>%
   distinct(site, year, date_planted, date_harvested) %>%
   
   left_join(fieldweather_submission1, by = c("site", "year")) %>%
-  filter(date >= date_planted,
-         date <= date_harvested) %>%
+  filter(date >= date_planted, date <= date_harvested) %>%
   group_by(site, year, date_planted, date_harvested) %>%
   summarise(
     n_days = n(),
     sum_gdd = sum(gdd_rounded, na.rm = TRUE),
     across(
       c(dayl_s, srad_w_m_2, tmax_deg_c, tmin_deg_c, vp_pa),
-      mean, na.rm = TRUE
+      mean,
+      na.rm = TRUE
     ),
     sum_precip = sum(prcp_mm_day, na.rm = TRUE),
     .groups = "drop"
@@ -748,16 +722,12 @@ pred_yield <- predict(best_workflow, merged_submission) %>%
 
 # replacing all NA values with predicted value
 merged_submission_final <- merged_submission %>%
-  dplyr::mutate(
-    yield_mg_ha = dplyr::if_else(
-      is.na(yield_mg_ha),
-      pred_yield,
-      yield_mg_ha
-    )
-  ) %>% 
+  dplyr::mutate(yield_mg_ha = dplyr::if_else(is.na(yield_mg_ha), pred_yield, yield_mg_ha)) %>%
   dplyr::select(year, site, hybrid, yield_mg_ha)
 
-write_csv(merged_submission_final,
-          here::here("teach_output", "csv", "merged_submission_final.csv"))
+write_csv(
+  merged_submission_final,
+  here::here("teach_output", "csv", "merged_submission_final.csv")
+)
 
 #knitr::purl("project_code_teach.qmd", output = "project_code_teach_final.R", documentation = 0)
